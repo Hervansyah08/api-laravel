6452cb869e91c3de174ab1d7769588244b5b42ff
hervansyah08 <hervansyahakbar@gmail.com>
2024-11-03 18:44:35 +0700

    membuat method crud

--- app/Http/Controllers/Api/PersonController.php
+++ app/Http/Controllers/Api/PersonController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Person;
+use Illuminate\Support\Facades\Validator;
 
 class PersonController extends Controller
 {
@@ -12,7 +14,12 @@ class PersonController extends Controller
      */
     public function index()
     {
-        //
+        $person =  Person::all();
+        return response()->json([
+            'status' => true,
+            'message' => 'Semua Data Person ',
+            'data' => $person,
+        ], 201);
     }
 
     /**
@@ -20,30 +27,77 @@ class PersonController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validasi error',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $person = Person::create($request->all());
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Data Person berhasil disimpan',
+            'data' => $person,
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Person $person)
     {
-        //
+        return response()->json([
+            'status' => true,
+            'message' => 'Data Person Berhasil Ditemukan',
+            'data' => $person,
+        ], 201);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Person $person)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validasi error',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $person->update($request->all());
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Data Person berhasil diupdate',
+            'data' => $person,
+        ], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Person $person)
     {
-        //
+        $person->delete();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Data Person berhasil dihapus',
+        ], 204);
     }
 }
